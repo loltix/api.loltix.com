14e7def5a66b80b554c7cba47d042c798ba39086
Alex Ebadirad <alex@ebadirad.com>
2020-03-11 23:28:28 -0700

    First cut of api + swagger docs

---
+++ Models/User.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *      User:
+ *        type: object
+ *        required:
+ *          - name
+ *          - email
+ *        properties:
+ *          name:
+ *            type: string
+ *          email:
+ *            type: string
+ *            format: email
+ *            description: Email for the user, needs to be unique.
+ *        example:
+ *           name: Alexander
+ *           email: fake@email.com
+ */
+
+class User {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+  }
+}
+
+module.exports = User;
---
+++ Routes/index.js
@@ -0,0 +1,133 @@
+"use strict";
+// Node Modules
+const express = require("express");
+const router = express.Router();
+
+// App Modules
+const User = require('../Models/User');
+
+//swagger ui modules
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+
+// Swagger set up
+const options = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Loltix API Documentation",
+      version: "1.0.0",
+      description:
+        "LolTix",
+      license: {
+        name: "MIT",
+        url: "https://choosealicense.com/licenses/mit/"
+      },
+      contact: {
+        name: "Swagger",
+        url: "https://swagger.io",
+        email: "Info@SmartBear.com"
+      }
+    },
+    servers: [
+      {
+        url: "http://localhost:8000/api/v1"
+      }
+    ]
+  },
+  apis: [
+    "./Models/User.js", 
+    "./Routes/index.js"
+  ]
+};
+
+const specs = swaggerJsdoc(options);
+router.use("/docs", swaggerUi.serve);
+router.get(
+  "/docs",
+  swaggerUi.setup(specs, {
+    explorer: true
+  })
+);
+
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: User management
+ */
+
+/**
+ * @swagger
+ * path:
+ *  /users/:
+ *    post:
+ *      summary: Create a new user
+ *      tags: [Users]
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/User'
+ *      responses:
+ *        "200":
+ *          description: A user schema
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/User'
+ */
+router.post("/users", (req, res, next) => {
+  const { email, name } = req.body;
+  const user = new User(name, email);
+  res.json(user);
+});
+
+/**
+ * @swagger
+ * path:
+ *  /users/{userId}:
+ *    get:
+ *      summary: Get a user by id
+ *      tags: [Users]
+ *      parameters:
+ *        - in: path
+ *          name: userId
+ *          schema:
+ *            type: string
+ *          required: true
+ *          description: Id of the user
+ *      responses:
+ *        "200":
+ *          description: An users object
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/User'
+ */
+router.get("/users", (req, res, next) => {
+  const userOne = new User("Alex", "fake@gmail.com");
+  const userTwo = new User("Chad", "fakeagain@gmail.com");
+  res.json({ userOne, userTwo });
+});
+
+// catch 404 and forward to error handler
+router.use(function(req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// Error Handler
+router.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message
+    }
+  });
+});
+
+module.exports = router;
